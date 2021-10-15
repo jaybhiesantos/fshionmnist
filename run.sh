28b423496d697b98d906803b4586cb32c65b260e
jaybhiesantos <92415406+jaybhiesantos@users.noreply.github.com>
2021-10-15 21:05:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip3 install pandas numpy matplotlib random sklearn seaborn;
+pip3 install pandas numpy matplotlib random sklearn;
+pip3 install seaborn;
 python3 ./fshionmnist/Fashion_MNIST.py;
