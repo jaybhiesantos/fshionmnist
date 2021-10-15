b420f5dcbb78c54b3d8c49c786ead96c7b9dabe7
jaybhiesantos <92415406+jaybhiesantos@users.noreply.github.com>
2021-10-15 20:36:24 +0800

    Add files via upload

---
+++ Fashion_MNIST.py
@@ -0,0 +1,378 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # CASE STUDY: FASHION CLASS CLASSIFICATION 
+# # Dr. Ryan Ahmed
+
+# # STEP #1: PROBLEM STATEMENT AND BUSINESS CASE
+
+# Fashion training set consists of 70,000 images divided into 60,000 training and 10,000 testing samples. Dataset sample consists of 28x28 grayscale image, associated with a label from 10 classes. 
+# 
+# The 10 classes are as follows:  
+# 0 => T-shirt/top
+# 1 => Trouser
+# 2 => Pullover
+# 3 => Dress
+# 4 => Coat
+# 5 => Sandal
+# 6 => Shirt
+# 7 => Sneaker
+# 8 => Bag
+# 9 => Ankle boot
+# 
+# Each image is 28 pixels in height and 28 pixels in width, for a total of 784 pixels in total. Each pixel has a single pixel-value associated with it, indicating the lightness or darkness of that pixel, with higher numbers meaning darker. This pixel-value is an integer between 0 and 255. 
+# 
+# ![image.png](attachment:image.png)
+# 
+
+# # STEP #2: IMPORTING DATA
+
+# In[1]:
+
+
+get_ipython().system('pip install seaborn')
+
+
+# In[2]:
+
+
+# import libraries 
+import pandas as pd # Import Pandas for data manipulation using dataframes
+import numpy as np # Import Numpy for data statistical analysis 
+import matplotlib.pyplot as plt # Import matplotlib for data visualisation
+import seaborn as sns
+import random
+
+
+# In[3]:
+
+
+# dataframes creation for both training and testing datasets 
+fashion_train_df = pd.read_csv('fashion-mnist_train.csv',sep=',')
+fashion_test_df = pd.read_csv('fashion-mnist_test.csv', sep = ',')
+
+
+# # STEP #3: VISUALIZATION OF THE DATASET  
+
+# In[4]:
+
+
+# Let's view the head of the training dataset
+# 784 indicates 28x28 pixels and 1 coloumn for the label
+# After you check the tail, 60,000 training dataset are present
+fashion_train_df.head()
+
+
+# In[5]:
+
+
+# Let's view the last elements in the training dataset
+fashion_train_df.tail()
+
+
+# In[6]:
+
+
+# Let's view the head of the testing dataset
+fashion_test_df.head()
+
+
+# In[7]:
+
+
+# Let's view the last elements in the testing dataset
+fashion_test_df.tail()
+
+
+# In[8]:
+
+
+fashion_train_df.shape
+
+
+# In[9]:
+
+
+# Create training and testing arrays
+training = np.array(fashion_train_df, dtype = 'float32')
+testing = np.array(fashion_test_df, dtype='float32')
+
+
+# In[10]:
+
+
+training.shape
+
+
+# In[11]:
+
+
+training
+
+
+# In[12]:
+
+
+testing
+
+
+# In[13]:
+
+
+# Let's view some images!
+i = random.randint(1,60000) # select any random index from 1 to 60,000
+plt.imshow( training[i,1:].reshape((28,28)) ) # reshape and plot the image
+
+plt.imshow( training[i,1:].reshape((28,28)) , cmap = 'gray') # reshape and plot the image
+
+
+# Remember the 10 classes decoding is as follows:
+# 0 => T-shirt/top
+# 1 => Trouser
+# 2 => Pullover
+# 3 => Dress
+# 4 => Coat
+# 5 => Sandal
+# 6 => Shirt
+# 7 => Sneaker
+# 8 => Bag
+# 9 => Ankle boot
+
+
+# In[14]:
+
+
+label = training[i,0]
+label
+
+
+# In[15]:
+
+
+# Let's view more images in a grid format
+# Define the dimensions of the plot grid 
+W_grid = 15
+L_grid = 15
+
+# fig, axes = plt.subplots(L_grid, W_grid)
+# subplot return the figure object and axes object
+# we can use the axes object to plot specific figures at various locations
+
+fig, axes = plt.subplots(L_grid, W_grid, figsize = (17,17))
+
+axes = axes.ravel() # flaten the 15 x 15 matrix into 225 array
+
+n_training = len(training) # get the length of the training dataset
+
+# Select a random number from 0 to n_training
+for i in np.arange(0, W_grid * L_grid): # create evenly spaces variables 
+
+    # Select a random number
+    index = np.random.randint(0, n_training)
+    # read and display an image with the selected index    
+    axes[i].imshow( training[index,1:].reshape((28,28)) )
+    axes[i].set_title(training[index,0], fontsize = 8)
+    axes[i].axis('off')
+
+plt.subplots_adjust(hspace=0.4)
+
+# Remember the 10 classes decoding is as follows:
+# 0 => T-shirt/top
+# 1 => Trouser
+# 2 => Pullover
+# 3 => Dress
+# 4 => Coat
+# 5 => Sandal
+# 6 => Shirt
+# 7 => Sneaker
+# 8 => Bag
+# 9 => Ankle boot
+        
+
+
+# # STEP #4: TRAINING THE MODEL
+
+# In[16]:
+
+
+# Prepare the training and testing dataset 
+X_train = training[:,1:]/255
+y_train = training[:,0]
+
+X_test = testing[:,1:]/255
+y_test = testing[:,0]
+
+
+# In[17]:
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size = 0.2, random_state = 12345)
+
+
+# In[18]:
+
+
+X_train.shape
+
+
+# In[19]:
+
+
+y_train.shape
+
+
+# In[20]:
+
+
+# * unpack the tuple
+X_train = X_train.reshape(X_train.shape[0], *(28, 28, 1))
+X_test = X_test.reshape(X_test.shape[0], *(28, 28, 1))
+X_validate = X_validate.reshape(X_validate.shape[0], *(28, 28, 1))
+
+
+# In[21]:
+
+
+X_train.shape
+
+
+# In[22]:
+
+
+X_test.shape
+
+
+# In[23]:
+
+
+X_validate.shape
+
+
+# In[24]:
+
+
+# open source Neural network library madke our life much easier
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential, load_model
+
+# y_train = keras.utils.to_categorical(y_train, 10)
+# y_test = keras.utils.to_categorical(y_test, 10)
+
+
+# In[25]:
+
+
+# Import train_test_split from scikit library
+# Import Keras
+
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import TensorBoard
+
+
+# In[26]:
+
+
+cnn_model = Sequential()
+
+# Try 32 fliters first then 64
+cnn_model.add(Conv2D(64,3, 3, input_shape = (28,28,1), activation='relu'))
+cnn_model.add(MaxPooling2D(pool_size = (2, 2)))
+
+cnn_model.add(Dropout(0.25))
+
+# cnn_model.add(Conv2D(32,3, 3, activation='relu'))
+# cnn_model.add(MaxPooling2D(pool_size = (2, 2)))
+
+cnn_model.add(Flatten())
+cnn_model.add(Dense(units = 32, activation = 'relu'))
+cnn_model.add(Dense(units = 10, activation = 'sigmoid'))
+
+
+# In[27]:
+
+
+cnn_model.compile(loss ='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001),metrics =['accuracy'])
+
+
+# In[28]:
+
+
+epochs = 50
+
+history = cnn_model.fit(X_train,
+                        y_train,
+                        batch_size = 512,
+                        nb_epoch = epochs,
+                        verbose = 1,
+                        validation_data = (X_validate, y_validate))
+
+
+# # STEP #5: EVALUATING THE MODEL
+
+# In[29]:
+
+
+evaluation = cnn_model.evaluate(X_test, y_test)
+print('Test Accuracy : {:.3f}'.format(evaluation[1]))
+
+
+# In[30]:
+
+
+# get the predictions for the test data
+predicted_classes = cnn_model.predict_classes(X_test)
+
+
+# In[31]:
+
+
+L = 5
+W = 5
+fig, axes = plt.subplots(L, W, figsize = (12,12))
+axes = axes.ravel() # 
+
+for i in np.arange(0, L * W):  
+    axes[i].imshow(X_test[i].reshape(28,28))
+    axes[i].set_title("Prediction Class = {:0.1f}\n True Class = {:0.1f}".format(predicted_classes[i], y_test[i]))
+    axes[i].axis('off')
+
+plt.subplots_adjust(wspace=0.5)
+
+
+# In[32]:
+
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, predicted_classes)
+plt.figure(figsize = (14,10))
+sns.heatmap(cm, annot=True)
+# Sum the diagonal element to get the total true correct values
+
+
+# In[33]:
+
+
+from sklearn.metrics import classification_report
+
+num_classes = 10
+target_names = ["Class {}".format(i) for i in range(num_classes)]
+
+print(classification_report(y_test, predicted_classes, target_names = target_names))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip3 install seaborn pandas numpy matplotlib random sklearn;
+python3 ./fshionmnist/Fashion_MNIST.py;
